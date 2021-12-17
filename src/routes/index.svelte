197bd60be9dcf06c43f48fc7809dbeeeebb55569
Brian McIlwain <technicallyemployed@gmail.com>
2021-12-17 15:44:42 -0600

    Try import styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
+<script>
+  // src/routes/__layout.svelte
+  import "../app.pcss";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
