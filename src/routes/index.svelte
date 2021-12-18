9cb36f28bf683e9a1c6532381495b7d6698dc99d
Brian McIlwain <technicallyemployed@gmail.com>
2021-12-17 21:25:26 -0600

    Formatting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
 <script>
   // src/routes/__layout.svelte
-  import "/static/app.css";
+  import '/static/app.css';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <!-- Suppress slot warning https://github.com/sveltejs/kit/issues/981 -->
-{#if false}<slot/>{/if}
+{#if false}<slot />{/if}
