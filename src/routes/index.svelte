c505f529018c6edc9aa4282f806848c92d874df3
Brian McIlwain <technicallyemployed@gmail.com>
2021-12-17 20:40:37 -0600

    Tempary hack silence warning

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,3 +5,6 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<!-- Suppress slot warning https://github.com/sveltejs/kit/issues/981 -->
+{#if false}<slot/>{/if}
