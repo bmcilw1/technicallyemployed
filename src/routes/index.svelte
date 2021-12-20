cc3f7e05e57707ce4ad843ec4791132250f393bc
Brian McIlwain <technicallyemployed@gmail.com>
2021-12-20 17:59:42 -0600

    Added layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,3 @@
-<script>
-  // src/routes/__layout.svelte
-  import '../app.css';
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
