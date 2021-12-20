91d5c3e36cc7bb3eb99ede978c9c4d43fc7918b1
Brian McIlwain <technicallyemployed@gmail.com>
2021-12-20 15:20:31 -0600

    Prefer defaults

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,36 +2,7 @@ const config = {
   mode: 'jit',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme('colors.gray.100'),
-            h1: {
-              color: theme('colors.gray.200'),
-            },
-            h2: {
-              color: theme('colors.gray.200'),
-            },
-            h3: {
-              color: theme('colors.gray.200'),
-            },
-            h4: {
-              color: theme('colors.gray.200'),
-            },
-            'ol > li::before': {
-              color: theme('colors.gray.300'),
-            },
-            'ul > li::before': {
-              backgroundColor: theme('colors.gray.300'),
-            },
-            a: {
-              color: theme('colors.pink.400'),
-            },
-          },
-        },
-      }),
-    },
+    extend: {},
   },
   // Only add this if you installed the TailwindCSS-plugins:
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
