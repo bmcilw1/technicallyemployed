a12c81813bf2cd88aeb8cff5cf7fe6484c2f778c
Brian McIlwain <technicallyemployed@gmail.com>
2021-12-20 21:23:27 -0600

    Simplify

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -24,9 +24,7 @@
           class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           aria-controls="mobile-menu"
           aria-expanded="false"
-          on:click={() => {
-            toggleExpandMenu();
-          }}
+          on:click={() => toggleExpandMenu()}
         >
           <span class="sr-only">Open main menu</span>
           <!--
