2f4ac3deacb23ad9679fbdb4526ad440d3a6f42d
Brian McIlwain <technicallyemployed@gmail.com>
2021-12-20 18:10:37 -0600

    Added success page

---
+++ src/routes/success.svelte
@@ -0,0 +1,14 @@
+<section class="container mx-auto h-screen flex flex-col justify-center items-center">
+  <h3 class="text-indigo-400">Welcome!</h3>
+  <p class="lg:mx-60 mx-5 my-12 text-center text-lg">
+    You're on the list! You'll receive an email the next time a newsletter comes out, usually once
+    every month or two. In the meantime, feel free to investigate the content here. Welcome!
+  </p>
+
+  <a
+    href="../"
+    class="flex-shrink-0 bg-purple-700 no-underline hover:border-purple-700 border text-white py-1 px-6 rounded"
+  >
+    Ok!
+  </a>
+</section>
