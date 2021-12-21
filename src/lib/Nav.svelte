5bc67f8c3222aa31f10c5548e17d858af29485b5
Brian McIlwain <technicallyemployed@gmail.com>
2021-12-20 20:22:31 -0600

    Fix simplify

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -107,7 +107,7 @@
   </div>
 
   <!-- Mobile menu, show/hide based on menu state. -->
-  <div class:hidden={expandMenu} class="sm:hidden" id="mobile-menu">
+  <div class:hidden={!expandMenu} class="sm:hidden" id="mobile-menu">
     <div class="px-2 pt-2 pb-3 space-y-1">
       <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
       <a
