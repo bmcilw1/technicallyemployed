907e4ac52d25aac48bfb3212c7f0608a13727921
Brian McIlwain <technicallyemployed@gmail.com>
2022-01-05 12:56:37 -0600

    Fix layout

---
+++ src/routes/coins/__layout.svelte
@@ -0,0 +1,40 @@
+<script context="module" lang="ts">
+  /**
+   * @type {import('@sveltejs/kit').Load}
+   */
+  export async function load({ url, fetch }) {
+    if (!url) return {};
+
+    const post = await fetch(`${url?.pathname}.json`).then((res) => res.json());
+
+    if (!post) {
+      return {
+        status: 404,
+        error: new Error('Post could not be found'),
+      };
+    }
+
+    return {
+      props: {
+        post,
+      },
+    };
+  }
+</script>
+
+<script lang="ts">
+  import PageHead from '$lib/components/PageHead.svelte';
+  import ArticleTitle from '$lib/components/ArticleTitle.svelte';
+  import ArticleMeta from '$lib/components/ArticleMeta.svelte';
+
+  export let post;
+</script>
+
+{#if post}
+  <PageHead title={post.name} description={post.ticker} />
+
+  <ArticleTitle title={post.name} />
+  <ArticleMeta author={post.name} date={post.date} />
+{/if}
+
+<slot />
