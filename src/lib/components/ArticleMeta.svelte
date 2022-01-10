f5f8c25345f67d087dfe2a325129d24bfa8d96b3
Brian McIlwain <technicallyemployed@gmail.com>
2022-01-10 15:16:57 -0600

    Prefer tailwind

--- src/lib/components/ArticleMeta.svelte
+++ src/lib/components/ArticleMeta.svelte
@@ -6,22 +6,6 @@
 </script>
 
 <p>
-  <span class="author">{author}</span>
-  <span class="date">{formattedDate}</span>
+  <span class="font-bold">{author}</span>
+  <span>{formattedDate}</span>
 </p>
-
-<style>
-  p {
-    margin: 0;
-    margin-bottom: calc(var(--spacing-unit) * 4);
-  }
-
-  .author {
-    font-weight: bold;
-    margin-right: calc(var(--spacing-unit) * 2);
-  }
-
-  .date {
-    color: var(--color-text-secondary);
-  }
-</style>
