3a37178383241e651271779a86b107398574898e
Brian McIlwain <technicallyemployed@gmail.com>
2022-04-04 21:53:26 -0500

    Better testing format

--- e2e/nav.spec.ts
+++ e2e/nav.spec.ts
@@ -9,14 +9,19 @@ test.describe('Nav', () => {
 
   test('Highlights activated item', async ({ page }) => {
     const logo = page.locator('[data-testid=logo]');
-    await expect(logo).toHaveText('Technically Employed');
     const homeLink = page.locator('[data-testid=nav-item-home]');
-    await expect(homeLink).toHaveClass(/activated/);
     const coinsLink = page.locator('[data-testid=nav-item-coins]');
+
+    await expect(logo).toHaveText('Technically Employed');
+    await expect(homeLink).toHaveClass(/activated/);
     await expect(coinsLink).not.toHaveClass(/activated/);
+
     await coinsLink.click();
     await expect(coinsLink).toHaveClass(/activated/);
+    await expect(homeLink).not.toHaveClass(/activated/);
+
     await logo.click();
     await expect(homeLink).toHaveClass(/activated/);
+    await expect(coinsLink).not.toHaveClass(/activated/);
   });
 });
