a669afd553c995fe599db2da4157fe984a58f711
Brian McIlwain <technicallyemployed@gmail.com>
2022-04-11 20:20:55 -0500

    Added additional test

---
+++ src/e2e/coins.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+const url = `localhost:3000/coins`;
+
+test.describe('Coins', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(url);
+  });
+
+  test('Shows and visits links', async ({ page }) => {
+    const btcLink = page.locator(`[data-testid=link-btc]`);
+    await Promise.all([page.waitForNavigation({ url: '**/bitcoin*' }), await btcLink.click()]);
+    expect(page.url()).toContain('bitcoin');
+  });
+});
--- src/lib/components/ArticleBlockLink.svelte
+++ src/lib/components/ArticleBlockLink.svelte
@@ -5,7 +5,11 @@
   export let accentDescription = '';
 </script>
 
-<a href={url} class="p-5 border rounded border-gray-200 hover:border-pink-400">
+<a
+  href={url}
+  class="p-5 border rounded border-gray-200 hover:border-pink-400"
+  data-testid={`link-${accentDescription.toLowerCase()}`}
+>
   <h3 class="text-slate-300">{title} →</h3>
   <p class="text-slate-300">
     {description}<span class="text-indigo-500 font-bold">{accentDescription}</span>
