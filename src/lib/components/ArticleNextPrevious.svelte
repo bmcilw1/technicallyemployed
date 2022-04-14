0c1aceb5be945a0e02b61452decee1a1edf81a3a
Brian McIlwain <technicallyemployed@gmail.com>
2022-04-13 20:45:26 -0500

    Additional coverage

--- src/lib/components/ArticleNextPrevious.svelte
+++ src/lib/components/ArticleNextPrevious.svelte
@@ -8,6 +8,7 @@
     <a
       href={previous.url}
       class="m-1 p-5 mx-auto sm:px-10 sm:ml-0 border rounded border-gray-200 hover:border-purple-400 text-slate-300"
+      data-testid="previous-article"
     >
       ← Previous: {previous.name}
     </a>
@@ -16,6 +17,7 @@
     <a
       href={next.url}
       class="m-1 p-5 mx-auto sm:px-10 sm:mr-0 sm:ml-auto border rounded border-gray-200 hover:border-purple-400 text-slate-300"
+      data-testid="next-article"
     >
       Next: {next.name} →
     </a>
