03ce40704166ee3f4605c2371b08756b4f142752
Brian McIlwain <technicallyemployed@gmail.com>
2022-04-14 19:00:40 -0500

    Added img

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,8 @@
   import profilePic from '/static/img/FormalProfessionalPic.jpg';
 </script>
 
+<img src={profilePic} />
+
 <section data-source={profilePic} class="relative bg-none z-0 h-screen">
   <h1>Tinkerings of a Software Enginner</h1>
 </section>
