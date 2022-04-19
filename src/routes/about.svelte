0127a1bf857baeecd9419ae9bf668e242ea89658
Brian McIlwain <technicallyemployed@gmail.com>
2022-04-18 21:16:12 -0500

    Added scaffold about

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<script>
+  import profilePic from '/static/img/FormalProfessionalPic.jpg';
+</script>
+
+<section class="my-3">
+  <div class="grid md:grid-flow-col mx-5">
+    <img src={profilePic} alt="Professional profile" class="w-full col-span-6" />
+    <div class="col-span-6 p-5">
+      <h1>Tinkerings of a Software Enginner</h1>
+    </div>
+  </div>
+</section>
