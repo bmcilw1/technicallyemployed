6949c309db151dcba14fa6f74e30bae907081ea7
Brian McIlwain <technicallyemployed@gmail.com>
2022-04-25 19:05:58 -0500

    Reduce about image

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,8 +4,12 @@
 
 <section class="my-3">
   <div class="grid md:grid-flow-col mx-5">
-    <img src={profilePic} alt="Professional profile" class="w-full col-span-6" />
-    <div class="col-span-6 p-5">
+    <img
+      src={profilePic}
+      alt="Professional profile"
+      class="h-32 w-32 overflow-hidden rounded-full"
+    />
+    <div class="p-5">
       <h1>Tinkerings of a Software Enginner</h1>
     </div>
   </div>
